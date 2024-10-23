b4ca30514bfe00d4d1921055b04c6ea90bb6e5df
jpreyesr <jp.reyesr1@uniandes.edu.co>
2024-10-23 10:56:26 -0500

    final posible

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,58 @@
 import { series } from './data.js';
-var seriesContainer = document.getElementById('series');
+var seriesTbody = document.getElementById('series');
 var totalSeries = document.getElementById("total-series");
-renderSeriesAsCards(series);
+var serieTitle = document.getElementById('serie-title');
+var serieImage = document.getElementById('serie-image');
+var serieDescription = document.getElementById('serie-description');
+var serieChannel = document.getElementById('serie-channel');
+renderCoursesInTable(series);
 totalSeries.innerHTML = "".concat(promedioSeries(series));
-function renderSeriesAsCards(series) {
-    console.log('Rendering series as cards');
+function renderCoursesInTable(series) {
+    console.log('Desplegando series');
     series.forEach(function (serie) {
-        // Create card element
-        var cardElement = document.createElement('div');
-        cardElement.classList.add('card', 'mb-3');
-        cardElement.style.width = '18rem';
-        // Card content
-        cardElement.innerHTML = "\n      <img src=\"".concat(serie.image, "\" class=\"card-img-top\" alt=\"").concat(serie.name, "\">\n      <div class=\"card-body\">\n        <h5 class=\"card-title\">").concat(serie.name, "</h5>\n        <p class=\"card-text\">").concat(serie.description, "</p>\n        <a href=\"").concat(serie.url, "\" target=\"_blank\" class=\"card-link\">").concat(serie.url, "</a>\n      </div>\n    ");
-        // Append the card to the container
-        seriesContainer.appendChild(cardElement);
+        var trElement = document.createElement("tr");
+        trElement.innerHTML = "<td>".concat(serie.id, "</td>\n                           <td><a href=\"#\" class=\"serie-link\" data-id=\"").concat(serie.id, "\">").concat(serie.name, "</a></td>\n                           <td>").concat(serie.channel, "</td>\n                           <td>").concat(serie.seasons, "</td>");
+        seriesTbody.appendChild(trElement);
     });
+    var links = document.querySelectorAll('.serie-link');
+    links.forEach(function (link) {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            var target = event.target;
+            var serieId = target.getAttribute('data-id');
+            if (serieId) {
+                showDetail(parseInt(serieId));
+            }
+        });
+    });
+}
+function showDetail(id) {
+    var selectedSerie = series.find(function (serie) { return serie.id === id; });
+    if (selectedSerie) {
+        // Mostrar título, imagen y descripción
+        serieTitle.innerText = selectedSerie.name;
+        serieImage.src = selectedSerie.image;
+        serieDescription.innerText = selectedSerie.description;
+        if (selectedSerie.url) {
+            serieChannel.innerHTML = "<a href=\"".concat(selectedSerie.url, "\" target=\"_blank\">").concat(selectedSerie.url, "</a>");
+        }
+        else {
+            serieChannel.innerText = selectedSerie.channel;
+            serieDescription.innerText = selectedSerie.description;
+        }
+        // Mostrar el modal
+        var modal = document.getElementById('serie-detail');
+        modal.style.display = 'block';
+    }
+}
+function closeModal() {
+    var modal = document.getElementById('serie-detail');
+    modal.style.display = 'none';
 }
 function promedioSeries(series) {
     var totalSeasons = 0;
     series.forEach(function (serie) { return totalSeasons += serie.seasons; });
     return totalSeasons / series.length;
 }
+window.showDetail = showDetail;
+window.closeModal = closeModal;
